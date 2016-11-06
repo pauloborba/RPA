c8160598989a678b8b25d17f8fc351da7e639a78
dgmneto <dgmn@cin.ufpe.br>
2016-11-06 00:09:35 -0300

    Apache poi foi adicionado em /lib e removido de BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,9 +72,6 @@ grails.project.dependency.resolution = {
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
 
-        //apache poi dependency
-        compile "org.grails.plugins:excel-import:1.0.0"
-
         // Plugins adicionados
         test(":cucumber:1.1.0") {
             exclude "spock-core"
