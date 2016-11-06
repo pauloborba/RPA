ab7d660151410f41d97223e87fb8c5f0d81cbe33
dgmneto <dgmn@cin.ufpe.br>
2016-11-06 00:13:01 -0300

    Refatoração para facilitar na leitura do código em QualisTestDataAndOperations

--- test/functional/steps/QualisTestDataAndOperations.groovy
+++ test/functional/steps/QualisTestDataAndOperations.groovy
@@ -9,7 +9,13 @@ class QualisTestDataAndOperations {
         def controller = new QualisController()
         controller.params << [title: title]
 
-        controller.request.addFile(new MockMultipartFile('qualis-sheet', filename, 'application/vnd.ms-excel', new FileInputStream(new File(System.getProperty('user.dir') + '/test/files/' + filename))))
+        if(filename != null) {
+            def path = System.getProperty('user.dir') + '/test/files/' + filename
+            def inputStream = new FileInputStream(new File(path))
+            def mockMultipart = new MockMultipartFile('qualis-sheet', filename, 'application/vnd.ms-excel', inputStream)
+            controller.request.addFile(mockMultipart)
+        }
+
         controller.save()
         controller.response.reset()
     }
