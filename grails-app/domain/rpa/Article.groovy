8b0afbed30c5f6df0ae93e461147ecd2f14f5b89
Rafael Albuquerque <rafaellsma@gmail.com>
2016-11-06 22:27:30 -0300

    [#133813957]Adicionar owner e override no equals no article

--- grails-app/domain/rpa/Article.groovy
+++ grails-app/domain/rpa/Article.groovy
@@ -1,14 +1,18 @@
 package rpa
 
+import groovy.transform.EqualsAndHashCode
+
+@EqualsAndHashCode(includes='tittle,journal,issn')
 class Article {
     String tittle
     String journal
     String issn
-    static belongsTo = Researcher
+    static belongsTo = [owner:Researcher]
 
     static constraints = {
         tittle(nullable: false, blank: false)
         journal(nullable: false, blank: false)
         issn(nullable: false, blank: false)
+        owner(nullable: true)
     }
 }
