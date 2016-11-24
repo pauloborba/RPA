0827adbf5fc8477e6f7f8cb37d9d2ab215979263
Rafael Albuquerque <rafaellsma@gmail.com>
2016-11-24 17:01:56 -0300

    Refatoração no XmlExtractor

--- grails-app/services/rpa/XmlExtractorService.groovy
+++ grails-app/services/rpa/XmlExtractorService.groovy
@@ -12,31 +12,32 @@ import org.xml.sax.SAXException;
 class XmlExtractorService {
     def getResearcher(InputStream file){
         Researcher researcher = new Researcher()
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(file);
-            if (document.getDocumentElement().getNodeName().equals("CURRICULO-VITAE")){
-                NodeList nodeList = document.getDocumentElement().getChildNodes();
-                for (int i = 0; i < nodeList.getLength(); i++) {
-                    Node node = nodeList.item(i);
-                    if (node instanceof Element) {
-                        if (node.getNodeName().equals("DADOS-GERAIS")) {
-                            researcher.name = node.getAttributes().getNamedItem("NOME-COMPLETO").getNodeValue();
-                            researcher.cpf = node.getAttributes().getNamedItem("CPF").getNodeValue();
-                        }
-                        else if(node.getNodeName().equals("PRODUCAO-BIBLIOGRAFICA")) {
-                            NodeList nodePubs  = node.getChildNodes();
-                            researcher.articles = getArticles(nodePubs, researcher);
+        Document document = buildDocument(file)
+        if (document.getDocumentElement().getNodeName().equals("CURRICULO-VITAE")){
+            NodeList nodeList = document.getDocumentElement().getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node instanceof Element) {
+                    if (node.getNodeName().equals("DADOS-GERAIS")) {
+                        researcher.name = node.getAttributes().getNamedItem("NOME-COMPLETO").getNodeValue();
+                        researcher.cpf = node.getAttributes().getNamedItem("CPF").getNodeValue();
+                    }
+                    else if(node.getNodeName().equals("PRODUCAO-BIBLIOGRAFICA")) {
+                        NodeList nodePubs  = node.getChildNodes();
+                        researcher.articles = getArticles(nodePubs, researcher);
 
-                        }
                     }
                 }
             }
-        }catch(ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
         }
         researcher
+    }
+
+    private Document buildDocument(InputStream file) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(file);
+        document
     }
 
     private Set<Article> getArticles(NodeList nodePubs, Researcher researcher) {
@@ -46,21 +47,12 @@ class XmlExtractorService {
             if (nodePub instanceof Element){
                 if(nodePub.getNodeName().equals("ARTIGOS-PUBLICADOS")) {
                     NodeList articlesNode = nodePub.getChildNodes();
-
                     for (int k = 0; k < articlesNode.getLength(); k++) {
                         Node articleNode = articlesNode.item(k);
                         NodeList details = articleNode.getChildNodes();
-                        Article article = new Article();
-                        for (int l = 0; l < details.getLength(); l++) {
-                            Node detail = details.item(l);
-                            if (detail.getNodeName().equals("DADOS-BASICOS-DO-ARTIGO")) {
-                                article.tittle = detail.getAttributes().getNamedItem("TITULO-DO-ARTIGO").getNodeValue()
-                            }else if(detail.getNodeName().equals("DETALHAMENTO-DO-ARTIGO")){
-                                article.issn = detail.getAttributes().getNamedItem("ISSN").getNodeValue()
-                                article.journal = detail.getAttributes().getNamedItem("TITULO-DO-PERIODICO-OU-REVISTA").getNodeValue()
-                            }
-                        }
+                        Article article = getArticle(details)
                         if(article.validate()){
+                            article.owner = researcher
                             articles.add(article)
                         }
                     }
@@ -68,5 +60,19 @@ class XmlExtractorService {
             }
         }
         articles
+    }
+
+    private Article getArticle(NodeList details) {
+        Article article = new Article();
+        for (int l = 0; l < details.getLength(); l++) {
+            Node detail = details.item(l);
+            if (detail.getNodeName().equals("DADOS-BASICOS-DO-ARTIGO")) {
+                article.title = detail.getAttributes().getNamedItem("TITULO-DO-ARTIGO").getNodeValue()
+            } else if (detail.getNodeName().equals("DETALHAMENTO-DO-ARTIGO")) {
+                article.issn = detail.getAttributes().getNamedItem("ISSN").getNodeValue()
+                article.journal = detail.getAttributes().getNamedItem("TITULO-DO-PERIODICO-OU-REVISTA").getNodeValue()
+            }
+        }
+        article
     }
 }
